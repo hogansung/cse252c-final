dfdacdda9f4c4760c60fb6a930f320dccbe37f6a
justin <jgorgen@eng.ucsd.edu>
2017-05-10 21:33:32 -0700

    Added 441-output correlation model

--- src/flownet.py
+++ src/flownet.py
@@ -1,39 +1,73 @@
 from keras.models import Model, Sequential
-from keras.layers import Activation, Input, Reshape, merge, Lambda, Dropout, Flatten, Dense
-from keras.layers.merge import add
+from keras.layers import Activation, Input, Reshape, merge, Lambda, Dropout, Flatten, Dense,LSTM
+from keras.layers.merge import add,concatenate,dot
 from keras.layers.convolutional import Convolution2D, Deconvolution2D, ZeroPadding2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.normalization import BatchNormalization
-from keras.layers.merge import Add
+from keras.layers.merge import Add,Concatenate,Dot
 import keras.backend as K
 import numpy as np
+import tensorflow as tf
 #from keras.utils.visualize_util import plot
+def myDot():
+    return Lambda(lambda x: tf.reduce_sum(tf.multiply(x[0],x[1]),axis=-1,keep_dims=True),name = 'myDot')
+
+
+def get_padded_stride(b,displacement_x,displacement_y,height_8=384/8,width_8=512/8):
+    slice_height = height_8- abs(displacement_y)
+    slice_width = width_8 - abs(displacement_x)
+    start_y = abs(displacement_y) if displacement_y < 0 else 0
+    start_x = abs(displacement_x) if displacement_x < 0 else 0
+    top_pad    = displacement_y if (displacement_y>0) else 0
+    bottom_pad = start_y
+    left_pad   = displacement_x if (displacement_x>0) else 0
+    right_pad  = start_x
+    
+    gather_layer = Lambda(lambda x: tf.pad(tf.slice(x,begin=[0,start_y,start_x,0],size=[-1,slice_height,slice_width,-1]),paddings=[[0,0],[top_pad,bottom_pad],[left_pad,right_pad],[0,0]]),name='gather_{}_{}'.format(displacement_x,displacement_y))(b)
+    return gather_layer
+
+def get_correlation_layer(conv3_pool_l,conv3_pool_r,max_displacement=20,stride2=2,height_8=384/8,width_8=512/8):
+    layer_list = []
+    dotLayer = myDot()
+    for i in range(-max_displacement, max_displacement+stride2,stride2):
+        for j in range(-max_displacement, max_displacement+stride2,stride2):
+            slice_b = get_padded_stride(conv3_pool_r,i,j,height_8,width_8)
+            current_layer = dotLayer([conv3_pool_l,slice_b])
+            layer_list.append(current_layer)
+    return Lambda(lambda x: tf.concat(3,x),name='441_output_concatenation')(layer_list)
+    
 
 def getModel(height = 384, width = 512):
 
     ## convolution model
 
-    # left model
+    # left and model
     input_l = Input(shape=(height, width, 3), name='pre_input')
-    conv1_l = Convolution2D(64, (7, 7), padding = 'same', name='conv1_l')(input_l)
-    conv1_l = MaxPooling2D(name='maxpool1_l')(conv1_l)
-    conv2_l = Convolution2D(128, (5, 5), padding = 'same', name='conv2_l')(conv1_l)
-    conv2_l = MaxPooling2D(name='maxpool2_l')(conv2_l)
-    conv3_l = Convolution2D(256, (5, 5), padding = 'same', name='conv3_l')(conv2_l)
-    conv3_l = MaxPooling2D(name='maxpool3_l')(conv3_l)
-
-    # right model
     input_r = Input(shape=(height, width, 3), name='nxt_input')
-    conv1_r = Convolution2D(64, (7, 7), padding = 'same', name='conv1_r')(input_r)
-    conv1_r = MaxPooling2D(name='maxpool1_r')(conv1_r)
-    conv2_r = Convolution2D(128, (5, 5), padding = 'same', name='conv2_r')(conv1_r)
-    conv2_r = MaxPooling2D(name='maxpool2_r')(conv2_r)
-    conv3_r = Convolution2D(256, (5, 5), padding = 'same', name='conv3_r')(conv2_r)
-    conv3_r = MaxPooling2D(name='maxpool3_r')(conv3_r)
+    #layer 1
+    conv1 = Convolution2D(64,(7,7), padding = 'same', name = 'conv1')
+    conv1_l = conv1(input_l)
+    conv1_r = conv1(input_r)
+    conv1_pool_l = MaxPooling2D(name='maxpool1_l')(conv1_l)
+    conv1_pool_r = MaxPooling2D(name='maxpool1_r')(conv1_r)
+    
+    #layer 2
+    conv2 = Convolution2D(128, (5, 5), padding = 'same', name='conv2')
+    conv2_l = conv2(conv1_pool_l)
+    conv2_r = conv2(conv1_pool_r)
+    conv2_pool_l = MaxPooling2D(name='maxpool2_l')(conv2_l)
+    conv2_pool_r = MaxPooling2D(name='maxpool2_r')(conv2_r)
+
+    #layer 3
+    conv3 = Convolution2D(256, (5, 5), padding = 'same', name='conv3_l')
+    conv3_l = conv3(conv2_pool_l)
+    conv3_r = conv3(conv2_pool_r)
+    conv3_pool_l = MaxPooling2D(name='maxpool3_l')(conv3_l)
+    conv3_pool_r = MaxPooling2D(name='maxpool3_r')(conv3_r)
 
     # merge
-    add_layer = add([conv3_l, conv3_r])
+    add_layer = get_correlation_layer(conv3_pool_l, conv3_pool_r,max_displacement=20,stride2=2,height_8=height/8,width_8=width/8)
 
     # merged convolution
     conv3_1 = Convolution2D(256, (3, 3), padding = 'same', name='conv3_1')(add_layer)
