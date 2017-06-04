a42cc1bf46773425d05fb0b259af447989c01407
Ubuntu <ubuntu@ip-172-31-34-226.us-west-2.compute.internal>
2017-06-04 01:55:52 +0000

    Added support for kitti generators

--- src/testModel.py
+++ src/testModel.py
@@ -1,4 +1,4 @@
-import flownet
+import trial
 import numpy as np
 import time
 import tensorflow as tf
@@ -6,21 +6,34 @@ import keras.backend as K
 
 
 if __name__=='__main__':
-    batch_size=5
+    batch_size=3
 
-    height = 160; width = 160;
+    height = 384; width = 512;
     start = time.time()
-    model = flownet.getModel(height=height,width=width,batch_size=batch_size,stateful=False)
+    model = trial.getModel(height=height,width=width,batch_size=batch_size,stateful=False)
     end = time.time()
     model_creation_time = end-start
     
     print "Time for model creation: {}. With batch size {}, height {}, width {}".format(model_creation_time, batch_size,height,width)
-    left_image = np.random.standard_normal((batch_size,height,width,3));
-    right_image = np.random.standard_normal((batch_size,height,width,3));
+    
+    left_image = np.random.standard_normal((batch_size,height,width,3))
+    right_image = np.random.standard_normal((batch_size,height,width,3))
     imu = np.random.standard_normal((batch_size,10,6))
+    ws = np.random.standard_normal((batch_size,3))
+    vs = np.random.standard_normal((batch_size,3))
+    M_inv = np.random.standard_normal((batch_size,4,4))
+
+    start = time.time()
+    foo = model.train_on_batch(x = [left_image,right_image,imu], y = [ws, vs, M_inv, M_inv])
+    end = time.time()
+    model_train_time = end- start
+    print "Time for training: {}. With batch size {}".format(model_train_time, batch_size)
+    exit(0)
+
     start = time.time()
     foo = model.predict_on_batch(x = [left_image,right_image,imu])
     end = time.time()
+
     model_predict_time = end- start
     print "Time for prediction: {}. With batch size {}, height {}, width {}".format(model_predict_time, batch_size,height,width)
     positions = foo[-1]
