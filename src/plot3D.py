a7d4536957717f56ae8a693c9048276bd8cd0d03
Ubuntu <ubuntu@ip-172-31-34-226.us-west-2.compute.internal>
2017-06-05 21:54:33 +0000

    update plot

--- src/plot3D.py
+++ src/plot3D.py
@@ -1,24 +1,25 @@
-import matplotlib as mpl
+import matplotlib
+matplotlib.use('Agg')
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.pyplot as plt
 
 def plot3D(data=[], labels=[]):
     # font size of the legend
-    mpl.rcParams['legend.fontsize'] = 10
+    matplotlib.rcParams['legend.fontsize'] = 10
 
     # create image object
     fig = plt.figure()
-    ax = fig.gca(projection='3d')
+    ax = fig.add_subplot(111, projection='3d')
 
     # plot something
     for datum, label in zip(data, labels):
         ax.plot(datum[:,0], datum[:,1], datum[:,2], label=label)
     
     # figure setting
-    ax.set_xlim(0, 2)
-    ax.set_ylim(0, 10)
-    ax.set_zlim(0, 10)
+    #ax.set_xlim(0, 2)
+    #ax.set_ylim(0, 10)
+    #ax.set_zlim(0, 10)
     ax.set_xlabel('X axis')
     ax.set_ylabel('Y axis')
     ax.set_zlabel('Z axis')
@@ -26,16 +27,27 @@ def plot3D(data=[], labels=[]):
 
     # plot figure
     plt.show()
+    plt.savefig('../res/3D.png')
 
 
 # read data
-files = []
-labels = []
-for fname in files:
-    with open(fname) as f:
-        f.readlines()
-#data = [np.array([[1,1,1], [1,2,2], [1,3,3]])]
-#labels = ['parametric curve']
+with open('../res/g_3D.txt') as f:
+    g_datum = []
+    for line in f.readlines():
+        line = map(float, line.strip().split(','))
+        g_datum.append(line)
+    g_datum = np.array(g_datum)
+
+with open('../res/v_3D.txt') as f:
+    v_datum = []
+    for line in f.readlines():
+        line = map(float, line.strip().split(','))
+        v_datum.append(line)
+    v_datum = np.array(v_datum)
+
+data = [g_datum, v_datum]
+labels = ['Ground Truth', 'VINet']
+
 
 # plot data
 plot3D(data, labels)
