1fac84ffc490b698c2e70c4fc3ce7f92ec757c0c
jgorgen <justin.gorgen@navy.mil>
2017-06-19 11:38:21 -0700

    Added script to automatically find tensorflow include dirs

--- tf/build_correlation_gpu.sh
+++ tf/build_correlation_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export TF_INC=/usr/local/lib/python2.7/dist-packages/tensorflow/include
+export TF_INC=`python -c "import tensorflow; print(tensorflow.sysconfig.get_include())"`
 nvcc -std=c++11 -c -o correlation_gpu.cu.o correlation_gpu.cu.cc -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 nvcc -std=c++11 -c -o correlation_grad_gpu.cu.o correlation_grad_gpu.cu.cc -I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
